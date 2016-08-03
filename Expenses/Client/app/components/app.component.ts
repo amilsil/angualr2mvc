1f75c898fea3d50f30d481d29f5717d97c0f2653
Amil Kumarasinghe <amil.kumarasinghe@crownbet.com.au>
2016-08-04 09:38:39 +1000

    bootstrap, new component, solution

---
+++ Expenses/Client/app/components/app.component.ts
@@ -0,0 +1,15 @@
+﻿import { Component } from '@angular/core';
+import { NavComponent } from './nav.component';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <mainNav></mainNav>
+        <h1>{{ title }}</h1>
+        `,
+    directives: [NavComponent]
+})
+
+export class AppComponent {
+    title = 'App expenses'
+ }
